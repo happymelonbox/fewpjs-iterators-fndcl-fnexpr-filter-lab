b798b0489a15dae919d4ad1bc47b7693b069232c
Justin Berger Howes <howes.j.j@gmail.com>
2021-05-16 22:23:26 +1000

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solution here
+
+
+function findMatching(drivers, string){
+    let bobbies = drivers.filter(word => word === string || word === string.toLowerCase())
+    return bobbies
+}
+
+function fuzzyMatch(drivers, letters){
+    let who = drivers.filter(name => name.startsWith(letters))
+    return who
+}
+
+function matchName(drivers, name){
+    let thatGuy = drivers.filter(him => him.name === name)
+    return thatGuy
+}
